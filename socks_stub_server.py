9f6257deb8e513da8abc16eddc48112a8b7a1cbf
susu <u1240976@gmail.com>
2015-12-22 17:44:11 +0800

    socks_stub_server.py: add comments and organize the code.

--- socks_stub_server.py
+++ socks_stub_server.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python2.7
-#encoding=utf-8
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 
 """
 Simple Socks Server implementation from http://code.google.com/p/python-socks5
@@ -11,8 +11,8 @@ from threading import Thread
 import sys
 import signal
 
-SOCKTIMEOUT=5#客户端连接超时(秒)
-RESENDTIMEOUT=300#转发超时(秒)
+SOCKTIMEOUT=5     # client connection timeout (sec)
+RESENDTIMEOUT=300 # resend timeout (sec)
 
 VER="\x05"
 METHOD="\x00"
@@ -29,6 +29,7 @@ UNASSIGNED="\x09"
 
 _LOGGER=None
 
+# Logger
 class Log:
     WARN="[WARN:]"
     INFO="[INFO:]"
@@ -52,8 +53,11 @@ def getLogger(output=sys.stdout):
         _LOGGER=SimpleLog(output)
     return _LOGGER
         
-
+# Socks Proxy
 class SocketTransform(Thread):
+    """
+    Proxy Implementation: using 2 threads.
+    """
     def __init__(self,src,dest_ip,dest_port,bind=False):
         Thread.__init__(self)
         self.dest_ip=dest_ip
@@ -71,6 +75,12 @@ class SocketTransform(Thread):
             self.dest.close()
 
     def resend(self):
+        """
+        Running 2 threads: 
+
+            First one read sock socket and write dest socket
+            Another one read dest socket and write dest socket
+        """
         self.sock=self.src
         self.dest=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.dest.connect((self.dest_ip,self.dest_port))
@@ -81,11 +91,20 @@ class SocketTransform(Thread):
         getLogger().write("Starting Resending")
         self.sock.settimeout(RESENDTIMEOUT)
         self.dest.settimeout(RESENDTIMEOUT)
-        Resender(self.sock,self.dest).start()
-        Resender(self.dest,self.sock).start()
+
+        # 2 threads:
+        #   self.sock => self.dest
+        #   self.dest => self.sock
+        Resender(self.sock, self.dest).start()
+        Resender(self.dest, self.sock).start()
 
 
 class Resender(Thread):
+    """
+    Running a thread to continuously read from src socket and write to dest socket.
+
+    src socket => dst socket
+    """
     def __init__(self,src,dest):
         Thread.__init__(self)
         self.src=src
@@ -109,53 +128,79 @@ class Resender(Thread):
         dest.close()
         getLogger().write("Client quit normally\n")
 
-
 def create_server(ip,port):
+    """
+    create Socks v5 Server
+
+    feature: listen multiple connection, multithread-based server
+    Proxy implementation is based on SocketTransform class
+    TODO: BIND, UDP Commands, Authentication
+    """
     transformer=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     transformer.bind((ip,port))
     signal.signal(signal.SIGTERM,OnExit(transformer).exit)
     transformer.listen(1000)
+
     while True:
         sock,addr_info=transformer.accept()
         sock.settimeout(SOCKTIMEOUT)
         getLogger().write("Got one client connection")
+
         try:
+            # Auth method
+            # | VER | NMETHODS | METHODS |
             ver,nmethods,methods=(sock.recv(1),sock.recv(1),sock.recv(1))
+                # auth response
+                # | VER | METHOD |
             sock.sendall(VER+METHOD)
-            ver,cmd,rsv,atyp=(sock.recv(1),sock.recv(1),sock.recv(1),sock.recv(1))
+            
+            # Socks Request
+            # | VER | CMD | RSV | ATYP | DST.ADDR | DST.PORT |
+            ver,cmd,rsv,addr_type=(sock.recv(1),sock.recv(1),sock.recv(1),sock.recv(1))
             dst_addr=None
             dst_port=None
-            if atyp=="\x01":#IPV4
+            
+            # parse destination address
+            if addr_type=="\x01": 
+                # IPv4
                 dst_addr,dst_port=sock.recv(4),sock.recv(2)
                 dst_addr=".".join([str(ord(i)) for i in dst_addr])
-            elif atyp=="\x03":#Domain
-                addr_len=ord(sock.recv(1))#域名的长度
+            elif addr_type=="\x03": 
+                # Domain Name
+                addr_len=ord(sock.recv(1)) # length of domain name
                 dst_addr,dst_port=sock.recv(addr_len),sock.recv(2)
                 dst_addr="".join([unichr(ord(i)) for i in dst_addr])
-            elif atyp=="\x04":#IPV6
+            elif addr_type=="\x04":
+                # IPv6
                 dst_addr,dst_port=sock.recv(16),sock.recv(2)
                 tmp_addr=[]
                 for i in xrange(len(dst_addr)/2):
                     tmp_addr.append(unichr(ord(dst_addr[2*i])*256+ord(dst_addr[2*i+1])))
                 dst_addr=":".join(tmp_addr)
+
             dst_port=ord(dst_port[0])*256+ord(dst_port[1])
+
             getLogger().write("Client wants to connect to %s:%d" %(dst_addr,dst_port))
             server_sock=sock
             server_ip="".join([chr(int(i)) for i in ip.split(".")])
 
-            if cmd=="\x02":#BIND
-                #Unimplement
-                sock.close()
-            elif cmd=="\x03":#UDP
-                #Unimplement
-                sock.close()
-            elif cmd=="\x01":#CONNECT
+            # processing Socks Request Commands
+            if cmd=="\x01":
+                # CONNECT Command
                 sock.sendall(VER+SUCCESS+"\x00"+"\x01"+server_ip+chr(port/256)+chr(port%256))
                 getLogger().write("Starting transform thread")
                 SocketTransform(server_sock,dst_addr,dst_port).start()
-            else:#Unspport Command
+            elif cmd=="\x02":
+                # TODO: BIND Command
+                sock.close()
+            elif cmd=="\x03":
+                # TODO: UDP ASSOCIATE Command
+                sock.close()
+            else:
+                # Unsupported Command
                 sock.sendall(VER+UNSPPORTCMD+server_ip+chr(port/256)+chr(port%256))
                 sock.close()
+
         except Exception,e:
             getLogger().write("Error on starting transform:"+e.message,Log.ERROR)
             sock.close()
